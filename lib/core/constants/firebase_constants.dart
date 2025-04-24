9011a95fd3b9234694354d488708f954992d7f34
mohamed <mmm.medox@gmail.com>
2025-04-24 19:40:42 +0200

    Remove Firebase files from tracking

--- lib/core/constants/firebase_constants.dart
+++
@@ -1,8 +0,0 @@
-class FirebaseConstants{
-  static const fireStoreVersionCollection = "VERSION";
-  static const fireStoreDeletesCollection = "DELETES";
-  static const fireStoreDataCollection = "DATA";
-  static const fireStoreCategoriesCollection = "CATEGORIES";
-  static const fireStoreArticlesCollection = "ARTICLES";
-  static const fireStoreAdminCollection ="ADMIN";
-}
